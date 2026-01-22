5ab8ee1a4a73bd1ef4e19cf732ad354b00289cac
Marc03067992 <greenflame76@gmail.com>
2026-01-22 09:33:27 -0600

    Actividad 2

---
+++ Nuestro_Codigo.py
@@ -0,0 +1,35 @@
+import random 
+
+def selection_sortPasoA(lista):
+    for i in range(len(lista)-1):    
+        for j in range(len(lista)-1-i):
+            print(lista)
+            if lista[j] > lista[j+1]:
+                lista[j], lista[j+1] = lista[j+1], lista[j]
+
+def selection_sortPasoD(lista):
+    for i in range(len(lista)-1):    
+        for j in range(len(lista)-1-i):
+            print(lista)
+            if lista[j] < lista[j+1]:
+                lista[j], lista[j+1] = lista[j+1], lista[j]
+
+
+continuar = True
+
+while continuar:
+    
+    print("Decide la manera en que saldrá la lista")
+    Decision = input("Descendente(D) / Ascendente(A): ").upper()
+    lista = [random.randint(1, 100) for x in range(5)]
+    lista2= list(lista)
+    
+    if Decision == "D":
+        selection_sortPasoD(lista)
+        
+        
+    elif Decision == "A": 
+        selection_sortPasoA(lista)
+        
+    continuar = input("¿Deseas salir(S/N)? ").upper() == "N"
+
